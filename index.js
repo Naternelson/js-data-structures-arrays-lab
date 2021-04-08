daae764454241e735c5ec14df74a6e31b06b1f4f
Nate <7nater.nelson@gmail.com>
2021-04-07 21:14:57 -0600

    commit tree 9d1901aa640187d87c4cade33379a3df5a0cf52c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+const drivers = "Milo Otis Garfield".split(' ');
+
+function destructivelyAppendDriver(name){
+    drivers.push(name);
+};
+
+function destructivelyPrependDriver(name){
+    drivers.unshift(name);
+};
+
+let destructivelyRemoveLastDriver = () => drivers.pop();
+let destructivelyRemoveFirstDriver = () => drivers.shift();
+let appendDriver = name => [...drivers, name];
+let prependDriver = name => [name, ...drivers];
+let removeLastDriver = name => drivers.slice(0, drivers.length - 1);
+let removeFirstDriver = name => drivers.slice(1);
